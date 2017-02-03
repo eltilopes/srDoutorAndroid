c9d1b41ff10c63c08754bb0bd09f6708185c287e
elto <eltilopes@gmail.com>
2017-02-02 22:28:29 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/srdoutorandroid/ListaMedicoActivity.java
@@ -0,0 +1,13 @@
+package br.com.srdoutorandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaMedicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_medico);
+    }
+}
