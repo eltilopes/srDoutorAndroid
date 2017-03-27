65391b3582f54a14865657453dd6676593b4cfeb
elto <eltilopes@gmail.com>
2017-03-27 20:14:35 -0300

    Alteraçoes nosso chate

--- app/src/main/java/br/com/srdoutorandroid/ListaMedicoActivity.java
+++ app/src/main/java/br/com/srdoutorandroid/ListaMedicoActivity.java
@@ -33,14 +33,14 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.crash.FirebaseCrash;
 
 import br.com.srdoutorandroid.activities.CustomizedListView;
 import br.com.srdoutorandroid.components.ProgressDialogAsyncTask;
 import br.com.srdoutorandroid.exception.AcessoNegadoException;
+import br.com.srdoutorandroid.model.User;
 import br.com.srdoutorandroid.util.ConexaoUtil;
 import br.com.srdoutorandroid.util.ToastUtil;
 import br.com.srdoutorandroid.util.ValidaUtil;
@@ -59,6 +59,7 @@ public class ListaMedicoActivity extends AppCompatActivity implements ProgressDi
     EditText senha;
     @Bind(R.id.dialog_progress)
     RelativeLayout layoutProgress;
+    private User user;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "ListaMedicoActivity";
@@ -67,21 +68,23 @@ public class ListaMedicoActivity extends AppCompatActivity implements ProgressDi
     private static final int RC_SIGN_IN_GOOGLE = 7859;
     private static final int RC_SIGN_IN_FACEBOOK = 64206;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_medico);
         ButterKnife.bind(this);
+        // FACEBOOK
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email", "public_profile");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                handleFacebookAccessToken(loginResult.getAccessToken());
+                accessFacebookLoginData(loginResult.getAccessToken());
             }
 
             @Override
@@ -97,18 +100,16 @@ public class ListaMedicoActivity extends AppCompatActivity implements ProgressDi
             }
         });
 
+        // GOOGLE SIGN IN
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-               // .requestIdToken("1030419986033-hro947juqboabffbqfh0bf0l7lsbdm16.apps.googleusercontent.com")
                 .requestIdToken("1030419986033-4d7akan3oo8l421h3re2e1e0vv29qtv2.apps.googleusercontent.com")
-               // .requestIdToken("1030419986033-hlovcvu42cvnj3s7imcqbu1s28ejhrhm.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
 
-        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         View.OnClickListener listener = new View.OnClickListener() {
@@ -121,155 +122,147 @@ public class ListaMedicoActivity extends AppCompatActivity implements ProgressDi
             }
         };
         signInButton.setOnClickListener(listener);
+
         mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        mAuthListener = getFirebaseAuthResultHandler();
+        initUser();
+    }
+    protected void initUser(){
+        user = new User();
+        user.setEmail( login.getText().toString() );
+        user.setPassword( senha.getText().toString() );
+    }
+
+    private FirebaseAuth.AuthStateListener getFirebaseAuthResultHandler(){
+        FirebaseAuth.AuthStateListener callback = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    String name = user.getDisplayName();
-                    String email = user.getEmail();
-                    Uri photoUrl = user.getPhotoUrl();
 
-                    // The user's ID, unique to the Firebase project. Do NOT use this value to
-                    // authenticate with your backend server, if you have one. Use
-                    // FirebaseUser.getToken() instead.
-                    String uid = user.getUid();
-                    // User is signed in
-                    ToastUtil.show(ListaMedicoActivity.this,  user.getDisplayName(), ToastUtil.INFORMATION);
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getDisplayName());
-                } else {
+                FirebaseUser userFirebase = firebaseAuth.getCurrentUser();
 
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                if( userFirebase == null ){
+                    return;
                 }
-                // ...
+
+                if( user.getId() == null
+                        && isNameOk( user, userFirebase ) ){
+
+                    user.setId( userFirebase.getUid() );
+                    user.setNameIfNull( userFirebase.getDisplayName() );
+                    user.setEmailIfNull( userFirebase.getEmail() );
+                    user.saveDB();
+                }
+
+                redirect();
             }
         };
-/**
- try {
- PackageInfo info = getPackageManager().getPackageInfo(
- "br.com.srdoutorandroid",
- PackageManager.GET_SIGNATURES);
- for (Signature signature : info.signatures) {
- MessageDigest md = MessageDigest.getInstance("SHA");
- md.update(signature.toByteArray());
- Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
- }
- } catch (NameNotFoundException e) {
-
- } catch (NoSuchAlgorithmException e) {
-
- }
- **/
-
-
+        return( callback );
     }
+
+    private boolean isNameOk( User user, FirebaseUser firebaseUser ){
+        return(
+                user.getName() != null
+                        || firebaseUser.getDisplayName() != null
+        );
+    }
+
+
+    private void accessFacebookLoginData(AccessToken accessToken){
+        accessLoginData(
+                "facebook",
+                (accessToken != null ? accessToken.getToken() : null)
+        );
+    }
+
+
+    private void accessLoginData( String provider, String... tokens ){
+        if( tokens != null
+                && tokens.length > 0
+                && tokens[0] != null ){
+
+            AuthCredential credential = FacebookAuthProvider.getCredential( tokens[0]);
+            credential = provider.equalsIgnoreCase("google") ? GoogleAuthProvider.getCredential( tokens[0], null) : credential;
+
+            user.saveProviderSP( ListaMedicoActivity.this, provider );
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if( !task.isSuccessful() ){
+                                ToastUtil.show(ListaMedicoActivity.this, "Login falhou!", ToastUtil.WARNING);
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            FirebaseCrash.report( e );
+                        }
+                    });
+        }
+        else{
+            mAuth.signOut();
+        }
+    }
+
+
 
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN_GOOGLE);
     }
 
-    private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
-
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            ToastUtil.show(ListaMedicoActivity.this, "Facebook login falhou!", ToastUtil.WARNING);
-                        }else{
-                            redirect();
-                        }
-
-                        // ...
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof FirebaseAuthUserCollisionException) {
-                    ToastUtil.show(ListaMedicoActivity.this, e.toString(), ToastUtil.WARNING);
-
-                } else if (e instanceof FirebaseAuthInvalidUserException) {
-                    ToastUtil.show(ListaMedicoActivity.this, e.toString(), ToastUtil.WARNING);
-                }
-            }
-            });
-    }
-
 
     @Override
-    public void onStart() {
-        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
-// See https://g.co/AppIndexing/AndroidStudio for more information.
-
-        mAuth.addAuthStateListener(mAuthListener);
-      //  mGoogleApiClient.connect();
+    protected void onStart() {
+        super.onStart();
+        verifyLogged();
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
+    private void verifyLogged(){
+        if( mAuth.getCurrentUser() != null ){
+            //callMainActivity();
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-       // mGoogleApiClient.disconnect();
+        else{
+            mAuth.addAuthStateListener( mAuthListener );
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if( mAuthListener != null ){
+            mAuth.removeAuthStateListener( mAuthListener );
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-       // callbackManager.onActivityResult( requestCode, resultCode, data );
         if( requestCode == RC_SIGN_IN_GOOGLE ){
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = result.getSignInAccount();
-                ToastUtil.show(ListaMedicoActivity.this, "Google login: " + account.getIdToken(), ToastUtil.INFORMATION);
-                firebaseAuthWithGoogle(account);
-            }else{
+
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent( data );
+            GoogleSignInAccount account = googleSignInResult.getSignInAccount();
+
+            if( account == null ){
                 ToastUtil.show(ListaMedicoActivity.this, "Google login falhou, tente novamente", ToastUtil.WARNING);
+                return;
             }
+
+            accessGoogleLoginData( account.getIdToken() );
         }
-        else if (requestCode == RC_SIGN_IN_FACEBOOK){
+        else{
             callbackManager.onActivityResult( requestCode, resultCode, data );
-        //   ToastUtil.show(ListaMedicoActivity.this, "Login facebook " , ToastUtil.INFORMATION);
-       }
+        }
     }
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            ToastUtil.show(ListaMedicoActivity.this, "Google login falhou!", ToastUtil.WARNING);
-                        }else{
-                            redirect();
-                        }
-                        // ...
-                    }
-                });
+    private void accessGoogleLoginData(String accessToken){
+        accessLoginData(
+                "google",
+                accessToken
+        );
     }
 
     public void logar(View view) {
