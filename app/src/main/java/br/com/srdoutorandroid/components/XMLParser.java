f62bb8ebcfc40beac6d2955f00c78eb4ecafcace
elto <eltilopes@gmail.com>
2017-04-13 02:16:17 -0300

    Imagem medico

--- app/src/main/java/br/com/srdoutorandroid/components/XMLParser.java
+++ app/src/main/java/br/com/srdoutorandroid/components/XMLParser.java
@@ -1,27 +1,24 @@
 package br.com.srdoutorandroid.components;
 
-import android.util.Log;
+/**
+ * Created by elton on 22/09/2016.
+ */
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+		import android.util.Log;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
+		import org.w3c.dom.Document;
+		import org.w3c.dom.Element;
+		import org.w3c.dom.Node;
+		import org.w3c.dom.NodeList;
+		import org.xml.sax.InputSource;
+		import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+		import java.io.IOException;
+		import java.io.StringReader;
+
+		import javax.xml.parsers.DocumentBuilder;
+		import javax.xml.parsers.DocumentBuilderFactory;
+		import javax.xml.parsers.ParserConfigurationException;
 
 public class XMLParser {
 
@@ -30,6 +27,10 @@ public class XMLParser {
 
 	}
 
+	/**
+	 * Getting XML from URL making HTTP request
+	 * @param url string
+	 * */
 	public String getXmlFromUrl(String url) {
 		String xml = null;
 
@@ -154,10 +155,6 @@ public class XMLParser {
 				"</music>";
 	}
 
-	/**
-	 * Getting XML DOM element
-	 * @param XML string
-	 * */
 	public Document getDomElement(String xml){
 		Document doc = null;
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -200,11 +197,6 @@ public class XMLParser {
 		return "";
 	}
 
-	/**
-	 * Getting node value
-	 * @param Element node
-	 * @param key string
-	 * */
 	public String getValue(Element item, String str) {
 		NodeList n = item.getElementsByTagName(str);
 		return this.getElementValue(n.item(0));
